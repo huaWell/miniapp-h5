3987e7db08417d444282742f012457090eb1f750
BG026 <806404356@qq.com>
2024-07-19 14:05:33 +0800

    add

--- src/views/OrderStats.vue
+++ src/views/OrderStats.vue
@@ -47,6 +47,15 @@
 
 <template>
 <div class="container">
+    <van-tree-select
+      :items="items"
+      :main-active-index="mainActiveIndex"
+      :active-id="activeId"
+      :max="max"
+      @click-nav="onClickNav"
+      @click-item="onClickItem"
+  />
+
     <div class="card" style="height: 28vh;">
       <div class="title">
         <div class="value">
@@ -164,7 +173,34 @@ export default {
                     ]
                 }
                 ]
-            }
+            },
+            mainActiveIndex: 0,
+            activeId: [],
+            max: 2,
+            items: [{name:"A",text:"产品", children: [{id:1,name:"A1",text:"产品1"},
+                                    {id:2,name:"A2",text:"产品2"},
+                                    {id:3,name:"A3",text:"产品3"},
+                                    {id:4,name:"A4",text:"产品4"},
+                                    {id:5,name:"A5",text:"产品5"},
+                                    {id:6,name:"A5",text:"产品6"},
+                                    {id:7,name:"A5",text:"产品7"},
+                                    {id:8,name:"A5",text:"产品8"},
+                                    {id:9,name:"A5",text:"其它"}]},
+            {name:"B",text:"状态", children: [{id: 10,name:"A1",text:"全部订单"},
+                                    {id: 11,name:"A1",text:"正常订单"},
+                                    {id: 12,name:"A1",text:"延误订单"}]},
+            {name:"C",text:"异常原因", children: [{id:13,name:"C1",text:"插单"},
+                                    {id:14,name:"C1",text:"飞单"},
+                                    {id:15,name:"C1",text:"其它"}]},
+            {name:"D",text:"客户", children: [{id:16,name:"A1",text:"客户1"},
+                                    {id:17,name:"A1",text:"客户2"},
+                                    {id:18,name:"A1",text:"客户3"},
+                                    {id:19,name:"A1",text:"客户4"},
+                                    {id:20,name:"A1",text:"客户5"}]},
+            {name:"E",text:"金额", children: [{id:21,name:"A1",text:"全部"},
+                                      {id:22,name:"A1",text:"1-1000万"},
+                                      {id:23,name:"A1",text:"1000-3000万"},
+                                      {id:24,name:"A1",text:">3000万"}]}]
         }
     },
     mounted(){
@@ -175,6 +211,19 @@ export default {
             this.$router.push({
                 path: "/orderProgress"
             })
+        },
+        onClickNav(index){
+          console.log(index)
+          this.mainActiveIndex = index;
+        },
+        onClickItem(item){
+          console.log(item)
+          const index = this.activeId.indexOf(item.id)
+          if (index>-1){
+            this.activeId.splice(index,1)
+          } else {
+            this.activeId.push(item.id)
+          }
         }
     }
 }
